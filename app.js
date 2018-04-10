b1639a7730db034ab0d4e12e18d36e24b5e975f5
kongbeoul <jik2586@gmail.com>
2018-04-10 10:35:40 +0900

    2018-04-10 업데이트

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const modify = require('./routes/modify')
 var app = express();
 
 // view engine setup
-app.set('view options', {layout:false})
+app.set('view options', { layout: false })
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile)
@@ -27,7 +27,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(__dirname+"/public"));
+app.use(express.static(__dirname + "/public"));
 
 app.use('/', index);
 // app.use('/page/view', view)
@@ -39,24 +39,24 @@ app.use('/modify', modify)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
-app.listen(3000, ()=>{
-  console.log('Server running Port at 3000')
+app.listen(8080, () => {
+    console.log('Server running Port at 3000')
 })
 
 
